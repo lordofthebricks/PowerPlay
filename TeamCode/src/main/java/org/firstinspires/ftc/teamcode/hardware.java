22de6b47407a4090eb844fac43d4d1799a0fc9f5
victor <therealpenguinolo@gmail.com>
2022-09-24 17:20:36 -0400

    added first hardware

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class hardware {
+
+    //motors
+    public DcMotor Right_Bottom;
+    public DcMotor Right_Top;
+    public DcMotor Left_Bottom;
+    public DcMotor Left_Top;
+    //vuforia
+
+    HardwareMap hwMap = null;
+
+    public void init(HardwareMap ahwMap){
+
+        hwMap = ahwMap;
+        Right_Bottom = hwMap.get(DcMotor.class, "Right_Bottom");
+        Right_Top = hwMap.get(DcMotor.class, "Right_Top");
+        Left_Bottom = hwMap.get(DcMotor.class, "Left_Bottom");
+        Left_Top = hwMap.get(DcMotor.class, "Left_Top");
+
+    }
+
+
+
+}
