76776f8518d598d5cf7d62f7c6fac07af149e29e
victor <therealpenguinolo@gmail.com>
2022-11-05 17:48:23 -0400

    work on Autonmous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testprograms/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testprograms/Teleop.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.testprograms;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 import org.firstinspires.ftc.teamcode.utils.hardware;
@@ -17,18 +18,26 @@ public class Teleop extends LinearOpMode {
         robot.init(hardwareMap);
 
         waitForStart();
-
+        robot.Slider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         while (opModeIsActive()) {
             robot.Left_Bottom.setPower(gamepad1.left_stick_y*0.7);
             robot.Right_Bottom.setPower(gamepad1.right_stick_y*0.7);
             robot.Left_Top.setPower(gamepad1.left_stick_y*0.7);
             robot.Right_Top.setPower(gamepad1.right_stick_y*0.7);
 
-            
+            double countsPerRotationSlide = 537.7;
+            int sliderRotations = 0;
             if(gamepad1.a){
-                robot.Slider.setPower(-0.7);
+
+                if (sliderRotations>0){
+                    robot.Slider.setTargetPosition((int) (robot.Slider.getCurrentPosition() - countsPerRotationSlide));
+                    sliderRotations--;
+                }
             }else if(gamepad1.x){
-                robot.Slider.setPower(0.7);
+                if (sliderRotations<9){
+                    robot.Slider.setTargetPosition((int) (robot.Slider.getCurrentPosition() + countsPerRotationSlide));
+                    sliderRotations++;
+                }
             }else{
                 robot.Slider.setPower(0.1);
             }
